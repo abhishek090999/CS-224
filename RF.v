19e021c1d8508b3ce0acb1779a754373431b0c0c
Abhishek kumar <53633852+abhishek090999@users.noreply.github.com>
2020-08-04 13:55:48 +0530

    Add files via upload

---
+++ RF.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ns
+module RF(clk, reset, rd_sel, wr_sel, wr_en, data_in, data_out);
+    input clk;
+    input reset;
+    input [2:0] rd_sel;
+    input [2:0] wr_sel;
+    input wr_en;
+    input [7:0] data_in;
+    output reg [7:0] data_out;
+    reg [7:0] r [7:0];
+    integer i;
+
+    always @(posedge clk)
+    begin
+	 if(wr_en)
+		r[wr_sel] <= data_in;
+        
+	 end
+
+    always @(*)
+    begin
+	 if(reset)
+		begin
+		  for(i = 0; i < 8; i = i + 1)
+			begin
+            r[i] <= 8'b0;
+			end
+		end 
+    data_out <= r[rd_sel];
+    end
+
+
+endmodule
